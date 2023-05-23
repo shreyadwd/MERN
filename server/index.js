9c07966e275b823a5080b1b78cd57240f559cada
Shreya Dwivedi <shreya.dwd03@gmail.com>
2023-05-23 13:24:32 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express=require('express')
+const app=express()
+const cors=require('cors')
+const mongoose=require('mongoose')
+const User= require('./models/user_model')
+const jwt= require('jsonwebtoken')
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27071/mernstack')
+
+app.post('/api/register', async (req,res) => {
+    console.log(req.body)
+    try{
+         await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({status:'ok'})
+    }catch(err){
+        console.log(err)
+        res.json({status:'error', error: 'Duplicate Email'})
+    }
+    
+})
+
+app.post('/api/login', async (req,res) => {
+    
+
+    
+         const user = await User.findOne({
+            email: req.body.email, 
+            password: req.body.password
+        })
+        if(user){
+            const token= jwt.sign({
+                name: user.name,
+                email: user.email,
+            }, 'secret123')
+            return res.json({status:'ok', user:token})
+        }else{
+            return res.json({status:'error', user:false})
+        }
+    
+})
+
+app.get('/api/quote', async (req,res) => {
+    
+    const token = req.headers['x-access-token']
+    
+    try{
+    const decoded= jwt.verify(token, 'secret123')
+    const email = decoded.email
+    const user = await User.findOne({email:email})
+
+    return res.json({status:'ok', quote: user.quote})
+    }catch(error){
+        console.log(error)
+        res.json({status:'error', error:'invalid token'})
+    }
+})
+
+app.post('/api/quote', async (req,res) => {
+    
+    const token = req.headers['x-access-token']
+    
+    try{
+    const decoded= jwt.verify(token, 'secret123')
+    const email = decoded.email
+     await User.updateOne({email:email}, {$set : {quote: req.body.quote}})
+
+    return res.json({status:'ok'})
+    }catch(error){
+        console.log(error)
+        res.json({status:'error', error:'invalid token'})
+    }
+})
+
+app.listen(1337, () => {
+    console.log("server started on 1337")
+})
